4b3f2c6beca9aa1d9406f82bf40b8366df1c877c
unknown <Anh>
2019-10-07 14:29:05 +1300

    fix

--- web/forms/sync-spreadsheetv4.php
+++ web/forms/sync-spreadsheetv4.php
@@ -1,5 +1,7 @@
 <?php
-require_once 'google-api-php-client-2.2.2/vendor/autoload.php';
+require 'google-api-php-client-2.2.2/vendor/autoload.php';
+require '../../vendor/autoload.php';
+
 
 putenv('GOOGLE_APPLICATION_CREDENTIALS='.__DIR__.'/client_secret.json');
 set_time_limit(0);
